3c46fc1e817d65d516dabf403348225d5fe93d5e
talhak1919 <132381293+talhak1919@users.noreply.github.com>
2023-08-22 22:35:13 +0500

    changes

--- lib/Screens/NetworkScreen.dart
+++ lib/Screens/NetworkScreen.dart
@@ -172,3 +172,337 @@ class MenuBar extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart'; // Import this for date formatting
+
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Form Example'),
+//         ),
+//         body: SingleChildScrollView(
+//           child: Column(
+//             children: [
+//               SizedBox(height: 32),
+//               HeaderSection(),
+//               SizedBox(height: 16),
+//               MyForm(),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class HeaderSection extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Stack(
+//           children: [
+//             Container(
+//               width: 231,
+//               height: 2,
+//               decoration: BoxDecoration(
+//                 color: Color(0xFFBEC5BE),
+//               ),
+//             ),
+//             Container(
+//               width: 120,
+//               height: 2,
+//               decoration: BoxDecoration(
+//                 color: Color(0xFF3D9F46),
+//               ),
+//             ),
+//             Container(
+//               width: 5.008691310882568,
+//               height: 7.095645427703857,
+//               // Add your other containers and shapes here
+//             ),
+//           ],
+//         ),
+//         SizedBox(height: 8), // Add space between lines and text
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Padding(
+//               padding: EdgeInsets.only(
+//                   right: 109), // Add left padding to "Exchange" text
+//               child: Text(
+//                 "Exchange",
+//                 style: TextStyle(
+//                   fontSize: 10,
+//                   fontWeight: FontWeight.w300,
+//                   color: Color(0xFF3D9F46),
+//                 ),
+//               ),
+//             ),
+//             Text(
+//               "Issues",
+//               style: TextStyle(
+//                   fontSize: 10,
+//                   fontWeight: FontWeight.w100,
+//                   color: Colors.grey),
+//             ),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// class MyForm extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(16.0),
+//       child: Column(
+//         children: [
+//           RoundedTextField(label: "Exchange", hintText: "Enter exchange name"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "CRF", hintText: "Enter CRF"),
+//           SizedBox(height: 35),
+//           RoundedTextField(
+//               label: "OR Reference", hintText: "Enter OR Reference"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "NOI", hintText: "Enter NOI"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Issue", hintText: "Enter Issue"),
+//           SizedBox(height: 35),
+//           RoundedDateField(label: "Date", hintText: "Enter date"),
+
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "CP Name", hintText: "Enter CP name"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "ID Pole", hintText: "Enter Id Pole"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Post Code", hintText: "Enter Post Code"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Latitude", hintText: "Enter Latitude"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Longitude", hintText: "Enter Longitude"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Adress", hintText: "Enter your Adress"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "DP Id", hintText: "Enter Dp Id"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Issue", hintText: "Enter issue"),
+//           SizedBox(height: 35),
+//           RoundedTextField(label: "Issue Note", hintText: "Enter Issue Note"),
+//           SizedBox(height: 35),
+//           RoundedTextField(
+//               label: "Exciting Wires", hintText: "Enter your Answer"),
+//           SizedBox(height: 35),
+//           RoundedTextField(
+//               label: "Forecast Wires", hintText: "Enter your Answer"),
+//           SizedBox(height: 35),
+//           RoundedTextField(
+//               label: "Furniture Issue", hintText: "Enter your Answer"),
+//           SizedBox(height: 35),
+//           RoundedTextField(
+//               label: "Maximun Pole Capacity", hintText: "Enter your Answer"),
+//           SizedBox(height: 35),
+//           Text("Picture",
+//               style: TextStyle(
+//                 fontSize: 14,
+//                 fontWeight: FontWeight.w400,
+//               )),
+//           SizedBox(height: 10), // Add some space below the "Picture" text
+//           Container(
+//             width: 250, // Adjust the width of the box
+//             height: 150, // Adjust the height of the box
+//             decoration: BoxDecoration(
+//               border: Border.all(
+//                 color: Color(0xFF3D9F46),
+//                 style: BorderStyle.solid,
+//                 width: 1,
+//               ),
+//             ),
+//             child: Center(
+//               child: IconButton(
+//                 icon: Icon(Icons.upload_file),
+//                 onPressed: () {
+//                   print("Upload image pressed");
+//                 },
+//               ),
+//             ),
+//           ),
+//           SizedBox(height: 20),
+//           Row(
+//             mainAxisAlignment:
+//                 MainAxisAlignment.spaceEvenly, // Adjust alignment as needed
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Handle the Next button press
+//                   // You can navigate to the next screen or perform an action here
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   primary: Color(0xffffffff), // Set the background color here
+//                 ),
+//                 child: Text(
+//                   "Cancel",
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                     fontWeight: FontWeight.w700,
+//                     color: Color(0xFF6EB544),
+//                   ),
+//                 ),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Handle the Next button press
+//                   // You can navigate to the next screen or perform an action here
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   primary: Color(0xFF6EB544), // Set the background color here
+//                 ),
+//                 child: Text(
+//                   "Next",
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                     fontWeight: FontWeight.w700,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class RoundedTextField extends StatelessWidget {
+//   final String label;
+//   final String hintText;
+//   final IconData? icon;
+
+//   RoundedTextField({required this.label, required this.hintText, this.icon});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Text(label),
+//         SizedBox(height: 4),
+//         Container(
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(10),
+//             border: Border.all(color: Color(0xFFC0D4AC)),
+//           ),
+//           child: TextFormField(
+//             decoration: InputDecoration(
+//               hintText: hintText,
+//               hintStyle: TextStyle(
+//                 fontWeight: FontWeight.w100,
+//                 fontSize: 14,
+//                 color: Colors.grey,
+//               ),
+//               contentPadding: EdgeInsets.all(10),
+//               border: InputBorder.none,
+//               suffixIcon: icon != null ? Icon(icon) : null,
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// class RoundedDateField extends StatefulWidget {
+//   final String label;
+//   final String hintText;
+
+//   RoundedDateField({required this.label, required this.hintText});
+
+//   @override
+//   _RoundedDateFieldState createState() => _RoundedDateFieldState();
+// }
+
+// class _RoundedDateFieldState extends State<RoundedDateField> {
+//   DateTime? selectedDate;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Text(widget.label),
+//         SizedBox(height: 4),
+//         Container(
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(10),
+//             border: Border.all(color: Color(0xFFC0D4AC)),
+//           ),
+//           child: InkWell(
+//             onTap: () async {
+//               final DateTime? picked = await showDatePicker(
+//                 context: context,
+//                 initialDate: DateTime.now(),
+//                 firstDate: DateTime(2000),
+//                 lastDate: DateTime(2101),
+//               );
+//               if (picked != null && picked != selectedDate) {
+//                 setState(() {
+//                   selectedDate = picked;
+//                 });
+//               }
+//             },
+//             child: InputDecorator(
+//               decoration: InputDecoration(
+//                 hintText: widget.hintText,
+//                 hintStyle: TextStyle(
+//                   fontWeight: FontWeight.w100,
+//                   fontSize: 14,
+//                   color: Colors.grey,
+//                 ),
+//                 contentPadding: EdgeInsets.all(10),
+//                 border: InputBorder.none,
+//                 suffixIcon: Icon(Icons.calendar_today), // Add the icon here
+//               ),
+//               child: Text(
+//                 selectedDate != null
+//                     ? DateFormat('yyyy-MM-dd').format(selectedDate!)
+//                     : '',
+//                 style: TextStyle(
+//                   fontWeight: FontWeight.w100,
+//                   fontSize: 14,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
